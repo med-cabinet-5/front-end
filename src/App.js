e2017f3b630096224eb9978de22daccab1707d63
Danika Thomson <danikathomson@Danikas-mac.local>
2020-02-04 16:56:33 +0100

    Login form draft

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import LoginForm from "./components/LoginForm"
 
 import './App.css';
 
@@ -6,6 +7,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className="login-form">
+          <LoginForm />
+        </div>
         <p>
           Med Cabinet
         </p>
